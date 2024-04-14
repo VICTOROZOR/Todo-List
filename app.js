0264f14bad8890c5d2dd6996de79985f2c56a2ce
Victor <93828164+VICTOROZOR@users.noreply.github.com>
2024-04-14 23:59:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const output = document.querySelector(".output");
+const message = document.querySelector(".message-container");
+
+function getTodo(value) {
+  let todo = document.createElement("div");
+  let textEle = document.createElement("span");
+  textEle.innerHTML = value;
+}
