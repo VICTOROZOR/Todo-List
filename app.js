21b10d7af5ab0ddad65d64b78c76ab2f3a84e1e6
Victor <93828164+VICTOROZOR@users.noreply.github.com>
2024-04-15 22:04:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,39 @@ function getTodo(value) {
   let todo = document.createElement("div");
   let textEle = document.createElement("span");
   textEle.innerHTML = value;
+  textEle.appendChild(textEle);
+  message.classList.toggle("success");
+  message.textContent = "Item Added";
+
+  setTimeout(() => {
+    message.classList.toggle("success");
+  }, 2000);
+
+  let closeEle = document.createElement("span");
+  closeEle.innerHTML = "&times";
+  closeEle.classList.add("delete");
+
+  closeEle.addEventListener("click", () => {
+    output.removeChild(todo);
+    message.classList.toggle("error");
+    message.textContent = "Item Deleted";
+
+    setTimeout(() => {
+      message.classList.toggle("error");
+    }, 2000);
+  });
+
+  todo.appendChild(closeEle);
+  todo.classList.add("todo");
+  return todo;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let value = input.value;
+  if (!value.trim()) return;
+  output.appendChild(getTodo(value));
+  input.value = "";
+});
+  
 }
